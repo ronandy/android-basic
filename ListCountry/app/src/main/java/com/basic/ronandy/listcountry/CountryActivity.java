c9d5601c32eb0cc61879997bcdfa3101b677e053
ronandy <ronandy@gmail.com>
2015-11-15 13:59:19 -0400

    Lista de Paises de sudamérica con territorio mayor a 1.000.000 Km.

---
+++ ListCountry/app/src/main/java/com/basic/ronandy/listcountry/CountryActivity.java
@@ -0,0 +1,66 @@
+package com.basic.ronandy.listcountry;
+
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CountryActivity extends ListActivity {
+
+    private ArrayList<String> results = new ArrayList<String>();
+    private String tableName = DBHelper.tableName;
+    private SQLiteDatabase newDB;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openAndQueryDatabase();
+
+        displayResultList();
+
+    }
+    private void displayResultList() {
+        TextView tView = new TextView(this);
+        tView.setText("Lista de Paises con superficies mayor a 1000000 km y filtrado de 5 resultados");
+        getListView().addHeaderView(tView);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, results));
+        getListView().setTextFilterEnabled(true);
+
+    }
+    private void openAndQueryDatabase() {
+        try {
+            DBHelper dbHelper = new DBHelper(this.getApplicationContext());
+            newDB = dbHelper.getWritableDatabase();
+            Cursor c = newDB.rawQuery("SELECT Nombre, Capital, Superficie FROM " +
+                    tableName +
+                    " where Superficie > 1000000 LIMIT 5", null);
+
+            if (c != null ) {
+                if (c.moveToFirst()) {
+                    do {
+                        String Nombre = c.getString(c.getColumnIndex("Nombre"));
+                        String Capital = c.getString(c.getColumnIndex("Capital"));
+                        Double Superficie = c.getDouble(c.getColumnIndex("Superficie"));
+                        results.add("Nombre: " + Nombre + "\nCapital: " + Capital + "\nSuperficie: " + Superficie);
+                    }while (c.moveToNext());
+                }
+            }
+        } catch (SQLiteException se ) {
+            Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+        } finally {
+            if (newDB != null)
+                newDB.execSQL("DELETE FROM " + tableName);
+            newDB.close();
+        }
+
+    }
+
+}
